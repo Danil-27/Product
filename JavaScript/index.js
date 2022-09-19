bd3c25e72f115b8eb28cc8dc953c31213c16de14
Danil-27 <pde200126mail.ru>
2022-09-20 00:13:15 +0300

    theme

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,6 +2,8 @@ const btn = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 const html = document.querySelector('#html__noscroll');
 const links = menu.querySelectorAll(".menu__link");
+const icon = document.querySelector(".icon");
+const menuBody = document.querySelector(".menu__body")
 
 let width =
   window.innerWidth ||
@@ -30,15 +32,24 @@ btn.addEventListener("click", () => {     // Если мы нажимаем на
   menu.classList.toggle("active");        // После нажития переопределяет класс
   btn.classList.toggle("active");         // После нажития переопределяет класс
   html.classList.toggle("noscroll");      // После нажития переопределяет класс
+  icon.classList.toggle("active");        // После нажития переопределяет класс
 });
 
 // Клик на пустую область 
 menu.addEventListener("click", (e) => {         
-   if (e.target.classList.contains("menu")) {   // contains() Этот метод при проверке наличия класса в списке возвращает булевы true или false .
+   if (width < 992 && e.target.classList.contains("menu")) {   // contains() Этот метод при проверке наличия класса в списке возвращает булевы true или false .
       menu.classList.remove("active");          // Если True , то Закрываем класс  
       btn.classList.toggle("active");           // Если True , то переопределяет класс
       html.classList.toggle("noscroll");        // Если True , то переопределяет класс
+      icon.classList.toggle("active");          // Если True , то переопределяет класс
+      // menuBody.classList.
    }
+   else (width < 992)  
+      // menu.classList.remove("active");          // Если false , то Закрываем класс  
+      // btn.classList.remove("active");           // Если false , то Закрываем класс  
+      // html.classList.toggle("noscroll");        // Если false , то Закрываем класс  
+      // icon.classList.remove("active");          // Если false , то Закрываем класс  
+   
 });
 
 //плавный переход 
@@ -54,6 +65,7 @@ links.forEach((link) => {
          menu.classList.remove("active");   // После нажития на ссылку закрывает меню 
          btn.classList.toggle("active");    // После нажития переопределяет класс
          html.classList.toggle("noscroll"); // После нажития переопределяет класс
+         icon.classList.toggle("active");   // После нажития переопределяет класс
     } 
   });
 });
---
+++ JavaScript/theme.js
@@ -0,0 +1,30 @@
+const htmlTheme = document.querySelector(".light");
+const theme = document.querySelector(".theme");
+const themeBackround = document.querySelector(".theme__backround");
+const decor = document.querySelector(".theme__decor");
+
+
+theme.addEventListener("click" , (e) => {
+   if (e.target.classList.contains("theme")) {
+      decor.classList.toggle("theme__decor");
+      decor.classList.toggle("decor");
+      htmlTheme.classList.toggle("light")
+      htmlTheme.classList.toggle("dark")
+   }
+})
+
+themeBackround.addEventListener("click" , (e) => {
+   if (e.target.classList.contains("theme__backround")) {
+      decor.classList.toggle("theme__decor");
+      decor.classList.toggle("decor");
+      htmlTheme.classList.toggle("light")
+      htmlTheme.classList.toggle("dark")
+   }
+})
+
+decor.addEventListener("click" , () => {
+   decor.classList.toggle("theme__decor");
+   decor.classList.toggle("decor");
+   htmlTheme.classList.toggle("light")
+   htmlTheme.classList.toggle("dark")
+});
