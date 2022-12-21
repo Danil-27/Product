c5ded5678bcc7b43bec493bcd574e331b371fbda
Danil-27 <pde200126mail.ru>
2022-12-21 21:53:14 +0300

    adjustment

--- JavaScript/swiper.js
+++ JavaScript/swiper.js
@@ -1,61 +1,61 @@
-new Swiper('.strategies__container ',{
-   breakpoints: {
+new Swiper('.swiper-container', {
+    breakpoints: {
       1: {
-         slidesPerView: 1,
-         slidesPerGroup: 1,
-         loopedSlides: 1,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        loopedSlides: 1,
       },
       576: {
-         slidesPerView: 2,
-         slidesPerGroup: 2,
-         loopedSlides: 2,
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        loopedSlides: 2,
       },
       992: {
-         slidesPerView: 3,
-         slidesPerGroup: 3,
-         loopedSlides: 3,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        loopedSlides: 3,
       },
-   },
-   pagination: {
+    },
+    pagination: {
       el: '.swiper-paginations',
       //булеты
       clickable: true,
-   },
-   // Автоматическая прокрутка
-   // autoplay: {
-   //    //Пауза между прокруткой
-   //    delay: 2500,
-   //    //Закончить на последнем слайде
-   //    stopOnLastSlide: true,
-   //    //Отключить после ручного переключения
-   //    disableOnInteraction: false, 
-   // },
-   //Отключаем предзагрузку картинки
-   preloadImeges: false,
-   //Подзагрузка картинок
-   lazy: {
-      //Подгружать на старте переключения слайда 
+    },
+    // Автоматическая прокрутка
+    // autoplay: {
+    //    //Пауза между прокруткой
+    //    delay: 2500,
+    //    //Закончить на последнем слайде
+    //    stopOnLastSlide: true,
+    //    //Отключить после ручного переключения
+    //    disableOnInteraction: false,
+    // },
+    //Отключаем предзагрузку картинки
+    preloadImeges: false,
+    //Подзагрузка картинок
+    lazy: {
+      //Подгружать на старте переключения слайда
       loadOnTransitionStart: false,
       //Подгружает предыдущие и следующие картинки
       loadPrevNext: false,
-   },
-   //Слежка за видимыми слайдами 
-   watchSlidesProgress: true,
-   //Добавление класса видимым слайдам 
-   watchSlidesVisibility: true,
-   //Быстрее пролистывает слайдеры
-   touchRatio: 2,
-   // Декоративный курсор
-   grabCursor: true,
-   //Кол-во слайдеров для показа
-   slidesPerView: 3,
-   //Кол-во пролистоваемых слайдеров 
-   slidesPerGroup: 3,
-   //Отступы
-   spaceBetween: 15,
-   //Бесконечный слайдер 
-   loop: true,
-   loopedSlides: 3,
-   //скорость
-   speed:1200,
-});
+    },
+    //Слежка за видимыми слайдами
+    watchSlidesProgress: true,
+    //Добавление класса видимым слайдам
+    watchSlidesVisibility: true,
+    //Быстрее пролистывает слайдеры
+    touchRatio: 2,
+    // Декоративный курсор
+    grabCursor: true,
+    //Кол-во слайдеров для показа
+    slidesPerView: 3,
+    //Кол-во пролистоваемых слайдеров
+  //   slidesPerGroup: 3,
+    //Отступы
+    spaceBetween: 15,
+    //Бесконечный слайдер
+    loop: true,
+    loopedSlides: 3,
+    //скорость
+    speed: 1200,
+  })
